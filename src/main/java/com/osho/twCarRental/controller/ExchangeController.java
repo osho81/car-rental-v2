6a6b4646ce661e864aed452d3dd5b64b8fa9bf08
osho81 <ossi.hour@gmail.com>
2022-10-29 03:48:56 +0200

    Working master branch with both eureka server and keycloak with functioning antMatchers

--- src/main/java/com/osho/twCarRental/controller/ExchangeController.java
+++ src/main/java/com/osho/twCarRental/controller/ExchangeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/exchange")
+@RequestMapping(path = "/api/v1/exchange")
 public class ExchangeController {
 
     @Autowired
